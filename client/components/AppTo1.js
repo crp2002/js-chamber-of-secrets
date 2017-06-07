7ff06623a1affa51ec22ea1e67ad8f25a1056a4a
Christian Pascual <c.pascual.7@gmail.com>
2017-06-07 19:02:40 -0400

    added challenges to mode1

--- client/components/AppTo1.js
+++ client/components/AppTo1.js
@@ -5,12 +5,66 @@ class AppTo1 extends Component{
     constructor(props){
         super(props);
         this.state={
-            questions: ['Write a function that returns a string that represents your first name','Sort [3, -1] in ascending order. Hint use sort()'],
-            value: ''
+            question1:"",
+            question2:""
         }
+        this.getForm1Value = this.getForm1Value.bind(this);
+        this.getForm2Value = this.getForm2Value.bind(this);
+    }
+    getForm1Value(e){
+        let selection1 = e.target.value;
+        if(selection1 === "0") this.setState({question1: true});
+        else this.setState({question1: false});
+    }
+
+    getForm2Value(e){
+        let selection2 = e.target.value;
+       if(selection2 === "number") this.setState({question2: true});
+       else this.setState({question2: false});
     }
 
     render(){
+
+        let message1, message2;
+        if(this.state.question1 === true){
+            message1 = (<em>that is CORRECT!!</em>);
+        }
+        if (this.state.question1 === false){
+            message1 = (<em>nope</em>);
+        }
+        if(this.state.question2 === true){
+            message2 = (<em>well DONE!!</em>);
+        }
+        if(this.state.question2 === false){
+            message2 = (<em>incorrect, try again...</em>);
+        }
+
+        return(
+        <div>    
+            <form onChange={this.getForm1Value}>
+                <p> Which one of the following is considered falsy in JavaScript? </p>
+                <input name="option" type="radio" value="[]" /> [] <br></br>
+                <input name="option" type="radio" value="-42" /> -42 <br></br>
+                <input name="option" type="radio" value="-Infinity" /> -Infinity <br></br>
+                <input name="option" type="radio" value="true" /> true <br></br>
+                <input name="option" type="radio" value="0" /> 0 (zero) <br></br>
+                {message1}
+            </form>
+            <form onChange={this.getForm2Value}>
+                <p> What will be the output of the following expression in JavaScript?</p>
+                <code>typeof NaN</code><br></br>
+                <input name="option2" type="radio" value="undefined" /> "undefined" <br></br>
+                <input name="option2" type="radio" value="object" /> object <br></br>
+                <input name="option2" type="radio" value="undefined" /> undefined <br></br>
+                <input name="option2" type="radio" value="number" /> 'number' <br></br>
+                <input name="option2" type="radio" value="null" /> null <br></br>
+                {message2}
+            </form>
+         </div> 
+        );
+    }
+
+    /*render(){
         return (
             <div>
             <Challenge questions={this.state.questions}/>
@@ -20,7 +74,7 @@ class AppTo1 extends Component{
             </form>    
             </div>
         )
-    }
+    }*/
 }
 
 class Challenge extends Component{
